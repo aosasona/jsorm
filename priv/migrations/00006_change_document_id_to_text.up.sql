217b369fc015a3cb0ef76599c593ca8747f9dd97
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2023-10-22 15:40:35 +0100

    Fixed schema and improved dev process

---
+++ priv/migrations/00006_change_document_id_to_text.up.sql
@@ -0,0 +1,15 @@
+DROP TABLE documents;
+
+CREATE TABLE documents (
+  id TEXT PRIMARY KEY NOT NULL,
+
+  content TEXT NOT NULL DEFAULT '{}' CHECK (json_valid(content)),
+  tags TEXT NOT NULL DEFAULT '[]' CHECK (json_valid(tags)),
+  created_at INTEGER NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at INTEGER NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+  user_id INTEGER NOT NULL,
+  parent_id TEXT,
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+  FOREIGN KEY (parent_id) REFERENCES documents (id) ON DELETE CASCADE
+)
