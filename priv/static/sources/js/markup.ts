70ce51029a85078ac82b23da8b63ab0bf7d94d3e
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2025-08-26 11:40:41 +0100

    Fix whitespace issue in preview

--- priv/static/sources/js/markup.ts
+++ priv/static/sources/js/markup.ts
@@ -1,81 +1,81 @@
 import * as toast from "./toast.js";
 
 export function _safeJSONParse(text: string): Record<string, unknown> | null {
-	try {
-		return JSON.parse(text);
-	} catch (e) {
-		toast.error("Invalid JSON", "3s");
-		return null;
-	}
+        try {
+                return JSON.parse(text);
+        } catch (e) {
+                toast.error("Invalid JSON", "3s");
+                return null;
+        }
 }
 
 function getIndentationClass(isNested: boolean): string {
-	return isNested ? "ml-4" : "";
+        return isNested ? "ml-4" : "";
 }
 
 export function toMarkUp(doc: Record<string, unknown>, isNested: boolean = false) {
-	let result = "";
-	for (const field in doc) {
-		if (typeof doc[field] === "object") {
-			if (Array.isArray(doc[field])) {
-				result += makeArrayMarkup(field, doc[field] as any[], isNested);
-			} else {
-				result += makeObjectMarkup(field, doc[field] as Record<string, unknown>, isNested);
-			}
-		} else {
-			result += makeFieldMarkup(field, doc[field] as string | number | boolean, isNested);
-		}
-	}
+        let result = "";
+        for (const field in doc) {
+                if (typeof doc[field] === "object") {
+                        if (Array.isArray(doc[field])) {
+                                result += makeArrayMarkup(field, doc[field] as any[], isNested);
+                        } else {
+                                result += makeObjectMarkup(field, doc[field] as Record<string, unknown>, isNested);
+                        }
+                } else {
+                        result += makeFieldMarkup(field, doc[field] as string | number | boolean, isNested);
+                }
+        }
 
-	return result;
+        return result;
 }
 
 function makeObjectMarkup(key: string, value: Record<string, unknown>, isNested: boolean): string {
-	let result = "";
-	if (value == null) {
-		return `<div class="${getIndentationClass(isNested)}"><b>${key}</b>: <span class="text-stone-600">null</span></div>`;
-	}
+        let result = "";
+        if (value == null) {
+                return `<div class="${getIndentationClass(isNested)}"><b>${key}</b>: <span class="text-stone-600">null</span></div>`;
+        }
 
-	result += `<div class="${getIndentationClass(isNested)}" id="object-markup-container" data-expanded="1">
+        result += `<div class="${getIndentationClass(isNested)}" id="object-markup-container" data-expanded="1">
   <div class="flex items-center m-0 -ml-1 cursor-pointer" id="object-markup-title">
     <i class="inline-block mr-1 text-lg text-yellow-400 ti ti-caret-down-filled" id="expanded-status-icon"></i>
     <b>${key}</b>
   </div>
 	<div id="object-markup">
   `;
-	result += toMarkUp(value, true);
-	result += "</div></div>";
-	return result;
+        result += toMarkUp(value, true);
+        result += "</div></div>";
+        return result;
 }
 
 function formatValue(value: string | number | boolean): string {
-	const t = typeof value;
-	let displayValue = value;
-	if (t === "boolean") {
-		displayValue = `<span class="text-${value ? "true" : "false"}">${value}</span>`;
-	} else if (t === "string") {
-		displayValue = `<span class="text-stone-400">"${value}"</span>`;
-	} else if (t === "number") {
-		displayValue = `<span class="text-yellow-500">${value}</span>`;
-	}
+        const t = typeof value;
+        let displayValue = value;
+        if (t === "boolean") {
+                displayValue = `<span class="text-${value ? "true" : "false"}">${value}</span>`;
+        } else if (t === "string") {
+                displayValue = `<span class="text-stone-400">"${value}"</span>`;
+        } else if (t === "number") {
+                displayValue = `<span class="text-yellow-500">${value}</span>`;
+        }
 
-	return displayValue?.toString();
+        return displayValue?.toString();
 }
 
 function makeFieldMarkup(key: string, value: string | number | boolean, isNested: boolean = false): string {
-	const displayValue = formatValue(value);
-	return `<div class="${getIndentationClass(isNested)}"><b>${key}</b>: ${displayValue}</div>`;
+        const displayValue = formatValue(value);
+        return `<div class="${getIndentationClass(isNested)}"><b>${key}</b>: ${displayValue}</div>`;
 }
 
 function makeArrayMarkup(key: string, value: any[], isNested: boolean): string {
-	let result = `<div class="${getIndentationClass(isNested)}"><b>${key}</b>: [`;
-	for (const [index, item] of value.entries()) {
-		if (typeof item === "object") {
-			result += makeObjectMarkup(index.toString(), item, true);
-			continue;
-		}
-		result += `<div class="${getIndentationClass(true)}">${formatValue(item)}</div>`;
-	}
-	result += "]</div>";
-	return result;
+        let result = `<div class="${getIndentationClass(isNested)}"><b>${key}</b>: [`;
+        for (const [index, item] of value.entries()) {
+                if (typeof item === "object") {
+                        result += makeObjectMarkup(index.toString(), item, true);
+                        continue;
+                }
+                result += `<div class="${getIndentationClass(true)}">${formatValue(item)}</div>`;
+        }
+        result += "]</div>";
+        return result;
 }
--- src/jsorm/pages/editor.gleam
+++ src/jsorm/pages/editor.gleam
@@ -33,7 +33,7 @@ fn preview_component() -> html.Node {
   section(
     [
       class(
-        "w-full h-[50vh] md:h-auto px-5 pt-4 lg:px-6 pb-32 md:pb-36 whitespace-pre-line overflow-y-auto border-l border-l-stone-800",
+        "w-full h-[50vh] md:h-auto px-5 pt-4 lg:px-6 pb-32 md:pb-36 overflow-y-auto border-l border-l-stone-800",
       ),
       id("preview"),
     ],
